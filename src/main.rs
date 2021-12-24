ed74fe01e985f698e55f68d47bef1f73b8c76b0e
JPSO <jpso.guerreiro@gmail.com>
2021-12-24 14:47:53 +0000

    ..

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,27 @@ use tiny_http::{
     Server,
     Response
 };
-extern crate argparse;
 
-use argparse::{
-    ArgumentParser, 
-    StoreTrue, 
-    Store
-};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap("CLI simple static file server", "0.1.0", "@J-P-S-O")]
+struct Args {
+    /// Name of the person to greet
+    #[clap(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[clap(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 
 
 fn main() {
     let mut host = "127.0.0.1";
     let mut port = 8080;
-    {  // this block limits scope of borrows by ap.refer() method
-        let mut ap = ArgumentParser::new();
-        ap.set_description("Host a static file webserver");
-        
-        ap.refer(&mut host)
-            .add_option(
-                &["--host"], 
-                StoreTrue,
-                "Host to bind"
-        );
-        ap.refer(&mut port)
-            .add_option(
-                &["--port"],
-                StoreTrue,
-                "Port to listen"
-        );
-        ap.parse_args_or_exit();
-    }
-    
+   
     // let mut server = Server::http();
 
     println!("{}:{}", host, port);
