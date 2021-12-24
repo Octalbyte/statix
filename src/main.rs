f854df77de3b3e2cc1cc80942957d64450794e52
JPSO <jpso.guerreiro@gmail.com>
2021-12-24 14:40:05 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,26 @@ use argparse::{
 
 
 fn main() {
+    let mut host = "127.0.0.1"
+    let mut port = 8080
+    {  // this block limits scope of borrows by ap.refer() method
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Host a static file webserver");
+        
+        ap.refer(&mut name)
+            .add_option(
+                &["--host"], 
+                Store,
+                "Host to bind"
+        );
+        ap.refer(&mut port)
+            .add_option(
+                &["--port"],
+                Store,
+                "Port to listen"
+        )
+        ap.parse_args_or_exit();
+    }
     
     let mut server = Server::http();
 
