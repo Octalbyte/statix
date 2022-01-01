307685213fb456d34c655fa30f8663b667b2adad
JPSO <jpso.guerreiro@gmail.com>
2022-01-01 20:52:49 +0000

    Transfer bad request to module

--- src/handler.rs
+++ src/handler.rs
@@ -1,1 +1,20 @@
 //include all the handlers here
+
+use tiny_http::{
+    Response,
+    Request,
+    Header,
+    StatusCode
+};
+
+pub fn badRequest (rq: Request){
+    rq.respond(
+        Response::from_string("<html><body><h1>BAD REQUEST :(</h1></body></html>")
+        .with_status_code(
+         StatusCode(500)
+      )
+      .with_header(
+         Header::from_bytes(&b"Content-Type"[..], &b"text/html"[..]).unwrap()
+      )
+     );
+}
--- src/main.rs
+++ src/main.rs
@@ -81,15 +81,7 @@ for _ in 0 .. 5 { //change this so user can choose threads
             let path = String::from(rq.url());
 
             if path.contains("../") || path.contains("\\") || path.contains(":") {
-               rq.respond(
-                   Response::from_string("<html><body><h1>BAD REQUEST :(</h1></body></html>")
-                   .with_status_code(
-                    StatusCode(500)
-                 )
-                 .with_header(
-                    Header::from_bytes(&b"Content-Type"[..], &b"text/html"[..]).unwrap()
-                 )
-                );
+                handler::badRequest(rq);
                 continue; //bad request
             }
 
