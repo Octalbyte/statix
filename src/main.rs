e95779772b53a0f7c0e4f27944aa6f1e01b22fa5
JPSO <jpso.guerreiro@gmail.com>
2022-01-07 15:01:18 +0000

    add repository, homepage fields, bump to 4.3.3

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ mod handler;
 mod lib;
 
 const about: &str = "Simple CLI static file server";
-const version: &str = "4.2.3";
+const version: &str = "4.3.3";
 const author: &str = "@Octalbyte";
 
 #[derive(Parser, Debug)]
