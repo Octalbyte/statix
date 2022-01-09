286a8e6e17144abebb60c463e11845e59ad49808
JPSO <jpso.guerreiro@gmail.com>
2022-01-09 16:54:45 +0000

    crt reading

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,22 @@
+use std::io;
+use std::io::Read;
+use std::io::BufReader;
+use std::fs::File;
+
 mod crt {
 
     pub fn public(_filename: String) -> Vec<u8> {
-        /*error supressor */
-        return vec![1, 2, 3];
+        let f = File::open("./".to_owned()+_filename).expect("Error opening public certificate file");
+        let mut reader = BufReader::new(f);
+        let mut buffer = Vec::new();
+        reader.read_to_end(&mut buffer).expect("Failed to tread to buffer");
+        return buffer;
     }
     pub fn private(_filename: String) -> Vec<u8> {
-        /* error suppressor */
-        return vec![1, 2, 3];
+        let f = File::open("./".to_owned()+_filename).expect("Error opening private key file");
+        let mut reader = BufReader::new(f);
+        let mut buffer = Vec::new();
+        reader.read_to_end(&mut buffer).expect("Failed to tread to buffer");
+        return buffer;
     }
 }
